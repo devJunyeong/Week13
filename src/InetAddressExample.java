20a60597eaba534c9cabf4a0892303b9cc31e124
Junyeon Hwang <202430334@email.daelim.ac.kr>
2024-11-20 15:04:54 +0900

    InetAddress

---
+++ src/InetAddressExample.java
@@ -0,0 +1,22 @@
+import java.net.*;
+
+public class InetAddressExample {
+    public static void main(String[] args) throws Exception {
+        InetAddress local = InetAddress.getLocalHost();
+
+        System.out.println("Local Host: " + local);
+        System.out.println("Local HostAddress: " + local.getHostAddress());
+        System.out.println("Local HostName: " + local.getHostName());
+        System.out.println("Local isAnyLocalAddress: " + local.isAnyLocalAddress());
+
+        System.out.println("Local getAddress: " + local.getAddress());
+
+        InetAddress google = InetAddress.getByName("www.naver.com");
+        System.out.println("NAVER IP: " + google);
+
+        InetAddress[] addresses = InetAddress.getAllByName("www.naver.com");
+        for (InetAddress addr : addresses) {
+            System.out.println("NAVER Address: " + addr);
+        }
+    }
+}
